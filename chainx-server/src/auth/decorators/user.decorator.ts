fc52371dc5735e6a5158b243591375f31f2066bf
KaliShau <fa6248122@gmail.com>
2025-03-03 20:50:39 +0300

    feature: add files api

---
+++ chainx-server/src/auth/decorators/user.decorator.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common'
+import { Users } from '@prisma/client'
+
+export const User = createParamDecorator(
+  (data: keyof Users, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest()
+    return data ? request.user[data] : request.user
+  }
+)
